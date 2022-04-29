366dd904ef886bcbe6fb73d78932ac2bf6786ae8
edar-dev <edoardo@arlati.dev>
2022-04-29 16:05:09 +0200

    added test for schema

---
+++ Services/building/test/schemas/create_house_schema_test.py
@@ -0,0 +1,42 @@
+import unittest
+
+from app.api.schemas import CreateHouseSchema
+
+
+class CreateHouseSchemaTestCase(unittest.TestCase):
+    def test_can_deserialize_schema(self):
+
+        payload = {
+                    "alias": "housealias",
+                    "description": "housedescription",
+                    "rooms": [
+                            {
+                                "alias": "roomalias",
+                                "description": "roomdescription"
+                            }
+                            ]
+                    }
+
+        house_domain = CreateHouseSchema().load(payload)
+
+        self.assertIsNotNone(house_domain)
+
+
+    def test_can_NOT_deserialize_schema(self):
+
+        payload = {
+                    "not_an_alias": "housealias",
+                    "description": "housedescription",
+                    "rooms": [
+                            {
+                                "alias": "roomalias",
+                                "description": "roomdescription"
+                            }
+                            ]
+                    }
+
+        with self.assertRaises(Exception) as context:
+            house_domain = CreateHouseSchema().load(payload)
+
+        self.assertTrue(context.exception)
+
